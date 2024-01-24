1b9b4038a749adcb14babecbaa689fb424c1af73
marcus0193 <marwanfaisal67@gmail.com>
2024-01-24 02:35:33 -0800

    fixx

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(count))
     for n in range(count):
-        print("{}: {}".format(n + 1, sys.argv[i + 1]))
+        print("{}: {}".format(n + 1, sys.argv[n + 1]))
