0af0c6744ebd20a281749b4779fe46c225f7ca6f
marcus0193 <marwanfaisal67@gmail.com>
2024-02-05 07:00:07 -0800

    last

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
     result = 0
-    for  in range(1, 3):
+    for n in range(1, 3):
         try:
             if n > a:
                 raise Exception('Too far')
