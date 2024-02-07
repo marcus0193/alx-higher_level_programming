1aab8d7b2a423e7ca5a85e3e2491e6b0b8358711
marcus0193 <marwanfaisal67@gmail.com>
2024-02-06 21:27:56 -0800

    fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,9 +5,9 @@ class square:
     """sqaure definations step"""
 
     def __init__(self, size=0):
-        """commites.
+        """retuen the size of a square.
 
-        arguements:
-            size: the length of the side of the square.
+            size
+                the length of the side of the square.
         """
         self.__size = size
