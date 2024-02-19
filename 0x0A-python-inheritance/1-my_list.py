c2285af1d04e2ed5215eb0e19f81cdc1e4b124ca
marcus0193 <marwanfaisal67@gmail.com>
2024-02-19 10:49:42 -0800

    fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,7 +2,7 @@
 '''Module for Mylist class.'''
 
 
-class Mylist(list):
+class MyList(list):
     '''Custom Mylist class.'''
     def print_sorted(self):
         '''Method for sorted lists printing.'''
