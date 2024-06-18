02eba917c0912ee6b58beaedce89806b61198d9b
marcus0193 <marwanfaisal67@gmail.com>
2024-06-18 15:55:13 -0700

    early fix

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,9 @@
 const args = process.argv.slice(2);
 const size = parseInt(process.argv[2], 10);
 
-if (args.length === 0) {
+if (isNaN(size)) {
+  console.log('Missing size');
+} else if (args.length === 0) {
   console.log('Missing size');
 } else if (size > 0) {
   const line = 'X'.repeat(size);
