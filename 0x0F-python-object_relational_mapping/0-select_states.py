d068e27bb7d6ae154685b2eb3c37c5d3bedaaec9
marcus0193 <marwanfaisal67@gmail.com>
2024-06-22 18:27:39 -0700

    f

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,12 +6,8 @@ def list_states(username, password, db_name):
     try:
         # Connect to MySQL server
         connection = MySQLdb.connect(host='localhost', port=3306,
-                                     user=root, passwd=root,db=hbtn_0e_0_usa)
+                                     user= "root", passwd= "root", db= "hbtn_0e_0_usa")
         cursor = connection.cursor()
-
-        # Debugging output
-        print(f"Connecting to MySQL server with user: {username}, db: {db_name}")
-
         # Execute query to retrieve states
         cursor.execute("SELECT * FROM states ORDER BY id ASC")
         states = cursor.fetchall()
